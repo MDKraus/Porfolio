baac8199a54435517ac623ad425e1037e0330214
Maxfield Kraus <maxfield.kraus@gmail.com>
2024-02-08 12:58:26 -0500

    fixed the content on the different pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,19 +1,13 @@
 export default function About() {
-    return (
-      <div>
-        <h1>About Page</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        As an Aspiring Full Stack Web Developer, I bring a diverse background in Graphic Design and Electromechanical Engineering to the dynamic realm of web development. Proficient in HTML, CSS, and JavaScript, I specialize in creating visually appealing and seamlessly interactive user interfaces. This technical proficiency extends to frameworks like React for crafting dynamic front-end experiences. My dedication to mastering the art of web development is driven by a desire to blend creativity with technical innovation.
+
+        With a foundation in Graphic Design; this multifaceted background allows me to approach web development with a unique lens, ensuring that each project not only meets technical requirements but also resonates aesthetically and functionally with end-users. Beyond the confines of traditional development, my passion for game design fuels my aspirations for a future where web development seamlessly converges with the immersive world of gaming.
+
+        In my pursuit of becoming a Full Stack Web Developer, I am a creative problem-solver committed to continuous learning. This commitment is reflected not only in my technical expertise but also in my ability to adapt and thrive in the ever-evolving landscape of web development. Join me on this exciting journey where technology, creativity, and innovation converge, and let's together shape engaging and impactful digital experiences.      </p>
+    </div>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,33 +8,29 @@ export default function Home() {
   return (
     <div className='highlight-box-container'>
       <div className="highlight-box">
-        <h1>Home Page</h1>
-        <p className='highlight-box-content'>
-          'INSERT BASIC INFO HERE'
-        </p>
-      </div>
-      <div className="highlight-box">
         <h2>About</h2>
         <p className='highlight-box-content'>
-          'INSERT BASIC INFO HERE'
+          My Name is Maxfield Kraus, I am striving to be a Frontend Web Developer
         </p>
       </div>
       <div className="highlight-box">
         <h2>Contact</h2>
         <p className='highlight-box-content'>
-          'INSERT BASIC INFO HERE'
+        <a href="https://github.com/MDKraus">Github</a><br/>
+        <a href="https://www.linkedin.com/in/maxfield-kraus/">Linkedin</a><br/>
+        <a href="mailto: maxfield.kraus@gmail.com">maxfield.kraus@gmail.com</a>
         </p>
       </div>
       <div className="highlight-box">
         <h2>Employment</h2>
         <p className='highlight-box-content'>
-          'INSERT BASIC INFO HERE'
+          
         </p>
       </div>
       <div className="highlight-box">
         <h2>Projects</h2>
         <p className='highlight-box-content'>
-          'INSERT BASIC INFO HERE'
+          I made this Portfolio Website as well
         </p>
       </div>
     </div>
